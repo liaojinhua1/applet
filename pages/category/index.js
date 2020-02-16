799d44f506751335dc34b010947c533a63783868
liaojinhua1 <Jin9Code@163.com>
2020-02-16 22:24:09 +0800

    完成分类页面以及导入字体图标库

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,4 +1,8 @@
 // pages/category/index.js
+// 导入request库
+import {
+  request
+} from "../../request/request.js"
 Page({
 
   /**
@@ -7,22 +11,11 @@ Page({
   data: {
     categoryData: [], //导航数据
     categoryList: [], //右侧的详细列表
-    currentIndex: 0, //当前点击的左侧导航栏的索引
-    currentId:'' //右侧导航，当前点击的导航id
+    selectIndex: 0, //默认选中第一个分类,当前点击的左侧导航栏的索引
+    scroll_top: 0 //滚动条的位置       
   },
 
-  // 点击了菜单导航
-  select: function(e) {
-    // console.log(e)
-    // 设置循环列表  
-    this.setData({
-      currentIndex: e.currentTarget.dataset.index,
-      categoryList: this.data.categoryData[e.currentTarget.dataset.index].children      
-    });
-    console.log(this.data.currentIndex)
-    console.log(this.data.categoryList)
-    // console.log(this.data.currentId)
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -30,31 +23,41 @@ Page({
     // 调用获取列表数据方法
     this.getCategoryData()
   },
-  // 发送请求，获取导航列表数据
-  getCategoryData() {
-    wx.showLoading({
-        title: '数据正在加载',
-      }),
-      // 发送请求，获取分类
-      wx.request({
-        url: 'https://api.zbztb.cn/api/public/v1/categories',
-        method: 'GET',
-        success: (res => {
-          this.setData({
-            categoryData: res.data.message,
-          });
-          this.setData({
-            categoryList: this.data.categoryData[0].children
-          })
-          console.log(res);
-          wx.hideLoading()
-        }),
-        fail: (err => {
-          console.log(err)
-        })
 
-        // complete: function(res) {},
-      })
+  // 发送请求，获取导航列表数据
+  getCategoryData: async function() {
+    const categoryData = await request({
+      url: "/categories"
+    });
+    const categoryList = categoryData[0].children
+    const selectIndex = 0;
+    // 把分类列表数据和时间存入本地
+    wx.setStorageSync("cate", {
+      time: Date.now(),
+      data: categoryData
+    })
+    this.setData({
+      categoryData,
+      categoryList,
+      selectIndex
+    })
+    console.log(categoryData);
+  },
+  // 点击了菜单导航
+  handleItemClick: function(e) {
+    //获取当前标题所在的行数
+    const index = e.currentTarget.dataset.index;
+    //设置 右边栏 滚动条的位置 默认是在顶部 值为0
+    const scroll_top = 0;
+    // 设置当前点击所在的行数以及右边栏的内容
+    this.setData({
+      selectIndex: index,
+      categoryList: this.data.categoryData[index].children,
+      scroll_top: scroll_top
+    });
+    // console.log(this.data.currentIndex)
+    // console.log(this.data.categoryList)
+    // console.log(this.data.currentId)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -66,8 +69,41 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
+  /* 
+  1. 每次拿到数据之后，先把数据存在存在缓存里面 storage
+  同时设置一下缓存时间
+  2. 当下次进来的时候
+    a.如果在有效时间内，我们就不发请求；
+    b.如果过了时间，那么就再次请求数据
+   */
+  /* 
+      1. 先从缓存中获取数据
+      2. 判断数据是否过期，如果过期重新请求数据
+      3. 如果没有过期 直接使用数据
+   */
   onShow: function() {
+    let cate = wx.getStorageSync("cate")
+    if (!cate) {
+      this.getCategoryData();
+    } else {
+      if (Date.now() - cate.data > 1000) {
+        this.getCategoryData();
+      } else {
+        // 没有过期，直接使用本地中现有数据
+        let categoryData = cate.data;
+        // 设置左边默认选项的下标
+        let selectIndex = 0;
+        // 设置右边列表的默认内容
+        let categoryList = categoryData[0].children;
+        // 设置到data中
+        this.setData({
+          categoryData,
+          categoryList,
+          selectIndex
+        })
 
+      }
+    }
   },
 
   /**
--- request/request.js
+++ request/request.js
@@ -4,28 +4,56 @@
   3. 使用promise 进行改造
  */
 // 引入基础地址
-import { BASE_URL } from "./urls.js";
-// 创建promise对象
-export const request = (params)=>{
-  // 加遮罩
+import {
+  BASE_URL
+} from "./urls.js";
+// // 创建promise对象
+// export const request = (params) => {
+//   // 加遮罩
+//   wx.showLoading({
+//     title: '正在加载中',
+//     mask: true
+//   })
+//   return new Promise(function(resolve, reject) {
+//     wx.request({
+//       //  结构传入的参数
+//       ...params,
+//       url: BASE_URL + params.url,
+//       success: (res) => {
+//         resolve(res.data.message);
+//       },
+//       fail: (err) => {
+//         reject(err);
+//       },
+//       // 隐藏遮罩
+//       // complete 接口调用结束的回调函数
+//       complete: () => {
+//         wx.hideLoading();
+//       }
+//     })
+//   })
+// }
+// 创建resquest对象
+export const request = (params) => {
+  // 添加遮罩，正在加载提示
   wx.showLoading({
-    title: '正在加载中',
-    mask:true
+    title: '正在加载',
+    mask: true
   })
-  return new Promise(function(resolve,reject){
+  // 返回一个promise对象
+  return new Promise(function(resolve, reject) {
     wx.request({
-      //  结构传入的参数
-      ...params,
+      // 对参数params进行解构
       url: BASE_URL + params.url,
-      success:(res)=>{
-        resolve(res.data.message);
+      success: (res) => {
+        resolve(res.data.message)
       },
-      fail:(err)=>{
-        reject(err);
+      fail: (err) => {
+        reject(err)
       },
-      // 隐藏遮罩
       // complete 接口调用结束的回调函数
-      complete:()=>{
+      // 请求结束后隐藏遮罩
+      complete: () => {
         wx.hideLoading();
       }
     })
